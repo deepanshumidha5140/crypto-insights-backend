93a48a1743b2fde35a793929f56bf22bff00f04c
deepanshumidha5140 <deepanshu_12015140@nitkkr.ac.in>
2025-06-30 14:46:19 +0530

    making CORS changes

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ app = FastAPI(
 # === CORS ===
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://crypto-insights-frontend.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
     allow_credentials=True,
